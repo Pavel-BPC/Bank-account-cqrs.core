501c2513975eb5a13b0bd1d17508cde7eb5cc90e
PavelBlinets <pavelblinets@gmail.com>
2021-12-21 14:02:52 +0300

    command dispatcher

--- src/main/java/com/techbank/cqrs/core/commands/BaseCommand.java
+++ src/main/java/com/techbank/cqrs/core/commands/BaseCommand.java
@@ -3,9 +3,11 @@ package com.techbank.cqrs.core.commands;
 import com.techbank.cqrs.core.messages.Message;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
+@SuperBuilder
 public class BaseCommand extends Message {
     public BaseCommand(String id) {
         super(id);
