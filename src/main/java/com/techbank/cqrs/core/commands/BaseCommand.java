e9bc269118749e416a76912725d0945f22f9c4c8
PavelBlinets <pavelblinets@gmail.com>
2021-12-22 16:25:47 +0300

    Aggregates

--- src/main/java/com/techbank/cqrs/core/commands/BaseCommand.java
+++ src/main/java/com/techbank/cqrs/core/commands/BaseCommand.java
@@ -1,15 +1,24 @@
 package com.techbank.cqrs.core.commands;
 
 import com.techbank.cqrs.core.messages.Message;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
+//@AllArgsConstructor
 @SuperBuilder
 public class BaseCommand extends Message {
     public BaseCommand(String id) {
         super(id);
+    }
+//
+//    public BaseCommand() {
+//    }
+//
+    public BaseCommand(MessageBuilder<?, ?> b) {
+        super(b);
     }
 }
